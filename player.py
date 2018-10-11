e86fdfca8a9695226352238390e13b9aae605f30
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-11 14:11:33 -0500

    starter code

---
+++ player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self,piece):
+        self.name = self.get.name()
+        self.piece = piece
+    def get_name(self):
+        name = input("what is your name-- ")
+    def get_choice(self, board):
+        pass
