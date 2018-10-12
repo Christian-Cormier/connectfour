ea959e3e0c8bc40cd3b6cbed16a0fdfc8a52607e
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-12 13:46:25 -0500

    possible complete

--- player.py
+++ player.py
@@ -4,5 +4,15 @@ class Player:
         self.piece = piece
     def get_name(self):
         name = input("what is your name-- ")
-    def get_choice(self, board):
-        pass
+    def get_choice(self, Board):
+        choice = input(int("where do you want to place your piece by column-- "))
+        if choice != int:
+            print('your choice has to be an integer')
+        if choice < 0:
+            print('your choice has to be greater than 0')
+            get_choice()
+        if choice > width:
+            print('your choice is greater than the width of the board')
+        else: 
+            return(choice)
+
