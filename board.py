87cb1729a2d7678520fee93eb7fc0e4c4cd95ce7
rachelh2071 <rachelh2071@gmail.com>
2018-10-12 14:12:37 -0500

    disp add and empty attempt

--- board.py
+++ board.py
@@ -2,32 +2,43 @@
 #from player import Player
 
 class Board:
-    def __init__(self):
-        self.width = int(input("How many columns are in your Connect Four board?"))
-        self.height = int(input("How many rows are in your Connect Four board?"))
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
         self.board = [[' ']*self.width for i in range(self.height)]
     
-    def place_piece(self, piece, col):
-        piece = Player.players[Game.turn//2].piece
-        col = Player.players[Game.turn//2].get_choice(self)
+    def add_piece(self, col, piece):
+        for row in range(len(self.board)):
+            for column in range((row)):
+                if col == column and self.board[row][col] != ' ':
+                    self.board[row-1][col] = piece
     
     def empty_board(self):
+        self.board = [[' ']*self.width for i in range(self.height)]
+    
+    def check_win(self):
         pass
     
-    def check_win():
+    def is_full(self):
         pass
     
-    def is_full():
-        pass
-    
-    def disp_board():
-        pass
+    def disp_board(self):
+        colnum = ""
+        print("--" * self.width)
+        for row in self.board:
+            for ele in row:
+                print(ele,end=' ')
+        print("--" * self.width)
+        for i in range(self.width):
+            colnum = (colnum + str(i+1)+' ')
+        print(colnum)
+            
 
 
 
 def main():
-    Board()
-    print(Board.board)
+    b = Board(6,7)
+    b.disp_board()
 
 
 if __name__ == "__main__":
