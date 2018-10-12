23437c9bcd95a64ea2d3e726eba58965a0c8088e
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-12 14:11:34 -0500

    fix

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ from board import Board
 from player import Player
 
 class Game:
-    
+    def __init__(self):
+        pass
+    def winner(self):
+        pass
+    def is_full(self):
+        pass
 if __name__ == "__main__":
     pass
