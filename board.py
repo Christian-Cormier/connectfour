d7a9a2c171a00509b8ef433404fdd4cf91bfacd2
rachelh2071 <rachelh2071@gmail.com>
2018-10-15 14:12:54 -0500

    check horiz win work

--- board.py
+++ board.py
@@ -8,16 +8,24 @@ class Board:
         self.board = [[' ']*self.width for i in range(self.height)]
     
     def add_piece(self, col, piece):
-        for row in range(len(self.board)):
-            for column in range((row)):
-                if col == column and self.board[row][col] != ' ':
-                    self.board[row-1][col] = piece
+        for row in range(self.height-1,-1,-1):
+            if self.board[row][col-1] == ' ':
+                self.board[row][col-1] = piece
+                break
+        else:
+            raise ValueError
     
     def empty_board(self):
         self.board = [[' ']*self.width for i in range(self.height)]
     
     def check_win(self):
-        pass
+        for i in range(self.height):
+            for c in range(self.width-4):
+                if self.board[i][c] == self.board[i][c+1] == self.board[i][c+2] == self.board[i][c+3] != ' ':
+                    print(True)
+                    break
+        
+                
     
     def is_full(self):
         pass
@@ -27,7 +35,8 @@ class Board:
         print("--" * self.width)
         for row in self.board:
             for ele in row:
-                print(ele,end=' ')
+                print(ele,end=" ")
+            print()        
         print("--" * self.width)
         for i in range(self.width):
             colnum = (colnum + str(i+1)+' ')
@@ -39,6 +48,13 @@ class Board:
 def main():
     b = Board(6,7)
     b.disp_board()
+    b.add_piece(3,'v')
+    b.add_piece(6,'v')
+    b.add_piece(2,'l')
+    b.add_piece(4,'v')
+    b.add_piece(5,'v')
+    b.disp_board()
+    b.check_win()
 
 
 if __name__ == "__main__":
