a2b30d4370e7f994af50bd30d039c92fc62c9bac
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-16 14:14:19 -0500

    basic

--- game.py
+++ game.py
@@ -3,10 +3,34 @@ from player import Player
 
 class Game:
     def __init__(self):
-        pass
+        self.turn = 0
+        self.players = []
+        self.players.append(Player('*'))
+        self.players.append(Player('$'))
+        self.board = Board(6,7)
+
     def winner(self):
-        pass
-    def is_full(self):
-        pass
+        if check_win() == True:
+            print("You Win!")
+        if check_win() == False:
+            #repeat
+            pass
+            
+    def check_full(self):
+        if is_full() == True:
+            print("You Tie!")
+        if is_full() == False:
+            #continue as normal
+            pass
+    def play_game(self):
+        print("Welcome To Connect 4!")
+        Player.get_choice(self,self.board)
+        turn = (turn + 1) % 2
+
 if __name__ == "__main__":
-    pass
+    g = Game()
+    g.play_game()
+    
+#while True:
+#    board.addpiece()
+    
