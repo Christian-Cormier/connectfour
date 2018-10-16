b2d813ce7adb30a0e41384d12d6b57100915166e
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-16 14:13:56 -0500

    more work

--- player.py
+++ player.py
@@ -1,18 +1,26 @@
 class Player:
     def __init__(self,piece):
-        self.name = self.get.name()
+        self.name = self.get_name()
+        #self.choice = self.get_choice(Board)
         self.piece = piece
     def get_name(self):
-        name = input("what is your name-- ")
-    def get_choice(self, Board):
-        choice = input(int("where do you want to place your piece by column-- "))
-        if choice != int:
-            print('your choice has to be an integer')
+        return input("what is your name-- ")
+    def get_choice(self,board):
+        choice = int(input(f'now {self.name} pick a column-- '))
+#        if choice != int:
+#            print('your choice has to be an integer')
+#            get_choice()
         if choice < 0:
             print('your choice has to be greater than 0')
             get_choice()
-        if choice > width:
+        if choice > board.width:     
             print('your choice is greater than the width of the board')
-        else: 
-            return(choice)
+            get_choice()
+        
 
+def main():
+    p1 = Player('x')
+    print(p1.get_choice(6))
+    
+if __name__ == "__main__":
+    main()
