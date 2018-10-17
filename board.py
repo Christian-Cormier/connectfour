8e9ef373400663b74b39f9d70dedad9515f7d4a2
rachelh2071 <rachelh2071@gmail.com>
2018-10-17 14:13:04 -0500

    only errors msg left

--- board.py
+++ board.py
@@ -8,6 +8,9 @@ class Board:
         self.board = [[' ']*self.width for i in range(self.height)]
     
     def add_piece(self, col, piece):
+        '''
+        
+'''
         for row in range(self.height-1,-1,-1):
             if self.board[row][col-1] == ' ':
                 self.board[row][col-1] = piece
@@ -19,31 +22,49 @@ class Board:
         self.board = [[' ']*self.width for i in range(self.height)]
     
     def check_win(self):
-        for i in range(self.height):
+        for i in range(self.height): #horizontal
             for c in range(self.width-4):
                 if self.board[i][c] == self.board[i][c+1] == self.board[i][c+2] == self.board[i][c+3] != ' ':
-                    print(True)
-                    break
-        for c in range(self.width):
+                    return True
+
+#        else:
+#            return False
+
+        for c in range(self.width): #vertical
             for r in range(self.height-3):
                 if self.board[r][c] == self.board[r+1][c] == self.board[r+2][c] == self.board[r+3][c] != ' ':
-                    print(True)
-                    break
-        for i in range(self.height-3):
+                    return True
+
+#        else:
+#            return False
+
+        for i in range(self.height-3): # positive diagonal
             for c in range(self.width-3):
                 if self.board[i][c] == self.board[i+1][c+1] == self.board[i+2][c+2] == self.board[i+3][c+3] != ' ':
-                    print(True)
-                    break
-        for i in range(self.height-3):
-            for c in range(self.width-3,self.height+1,-1):
+                    return True
+                
+
+#        else:
+#            return False
+
+        for i in range(self.height-4,self.height): #negative diagonal
+            for c in range(self.width-3):
                 if self.board[i][c] == self.board[i+1][c-1] == self.board[i+2][c-2] == self.board[i+3][c-3] != ' ':
-                    print(True)
-                    break
-        
+                    return True
+
+#        else:
+#            return False
+        return False
                 
     
     def is_full(self):
-        pass
+        count = 0
+        for i in range(self.width):
+            if self.board[0][i] != ' ':
+                count +=1
+        if count == self.width:
+            return True
+        return False
     
     def disp_board(self):
         colnum = ""
@@ -63,18 +84,19 @@ class Board:
 def main():
     b = Board(6,6)
     b.disp_board()
-    b.add_piece(1,'v')
-    b.add_piece(2,'t')
-    b.add_piece(2,'v')
-    b.add_piece(3,'t')
+    b.add_piece(4,'v')
     b.add_piece(3,'t')
     b.add_piece(3,'v')
-    b.add_piece(4,'t')
-    b.add_piece(4,'t')
-    b.add_piece(4,'t')
-    b.add_piece(4,'v')
+    b.add_piece(2,'t')
+    b.add_piece(2,'t')
+    b.add_piece(2,'v')
+    b.add_piece(1,'t')
+    b.add_piece(1,'t')
+    b.add_piece(1,'t')
+    b.add_piece(1,'v')
     b.disp_board()
     b.check_win()
+    
 
 
 if __name__ == "__main__":
