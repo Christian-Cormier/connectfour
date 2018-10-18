5ba8d6c1f4b78679da96397ec69cc1b82af91e2d
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-18 14:13:07 -0500

    'finished'

--- player.py
+++ player.py
@@ -1,7 +1,6 @@
 class Player:
     def __init__(self,piece):
         self.name = self.get_name()
-        
         self.piece = piece
     def get_name(self):
         return input("what is your name-- ")
