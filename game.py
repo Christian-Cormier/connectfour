5cf9c8158348aa034c97d23958b8b8c014a30a15
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-10-18 13:50:50 -0500

    'wip'

--- game.py
+++ game.py
@@ -2,33 +2,41 @@ from board import Board
 from player import Player
 
 class Game:
-    def __init__(self):
+    def __init__(self,board):
         self.turn = 0
         self.players = []
-        self.players.append(Player('*'))
-        self.players.append(Player('$'))
-        self.board = Board(6,7)
+        self.board = board
 
     def winner(self):
-        if check_win() == True:
+        if check_win():
             print("You Win!")
-        if check_win() == False:
-            #repeat
-            pass
+        
+            
             
     def check_full(self):
-        if is_full() == True:
+        if is_full():
             print("You Tie!")
-        if is_full() == False:
-            #continue as normal
-            pass
+        
     def play_game(self):
         print("Welcome To Connect 4!")
-        Player.get_choice(self,self.board)
-        turn = (turn + 1) % 2
-
+        print(' ')
+        print('Player 1 what is your name')
+        self.players.append(Player('*'))
+        print('Player 2 what is your name')
+        self.players.append(Player('$'))
+        while True:
+            try:
+                self.board.disp_board()
+                self.choice = Player.get_choice(self,self.board)
+                self.board.add_piece(self.choice,self.players[self.turn].piece)
+                if self.board.detect_win():
+                    return
+                if self.board.is_full():
+                    return
+            except Exception as e:
+                pass
 if __name__ == "__main__":
-    g = Game()
+    g = Game(Board(7,7))
     g.play_game()
     
 #while True:
