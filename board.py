97a5471121ef090d00f1749584d15cc3e43d2e0a
rachelh2071 <rachelh2071@gmail.com>
2018-10-18 13:29:40 -0500

    board doctest all pass

--- board.py
+++ board.py
@@ -8,15 +8,14 @@ class Board:
         self.board = [[' ']*self.width for i in range(self.height)]
     
     def add_piece(self, col, piece):
-        '''
-        
-'''
+        if col > self.width or col <= 0:
+            raise ValueError("Invalid Column")
         for row in range(self.height-1,-1,-1):
             if self.board[row][col-1] == ' ':
                 self.board[row][col-1] = piece
                 break
         else:
-            raise ValueError
+            raise ValueError("Column Full")
     
     def empty_board(self):
         self.board = [[' ']*self.width for i in range(self.height)]
@@ -26,36 +25,19 @@ class Board:
             for c in range(self.width-4):
                 if self.board[i][c] == self.board[i][c+1] == self.board[i][c+2] == self.board[i][c+3] != ' ':
                     return True
-
-#        else:
-#            return False
-
         for c in range(self.width): #vertical
             for r in range(self.height-3):
                 if self.board[r][c] == self.board[r+1][c] == self.board[r+2][c] == self.board[r+3][c] != ' ':
                     return True
-
-#        else:
-#            return False
-
         for i in range(self.height-3): # positive diagonal
             for c in range(self.width-3):
                 if self.board[i][c] == self.board[i+1][c+1] == self.board[i+2][c+2] == self.board[i+3][c+3] != ' ':
                     return True
-                
-
-#        else:
-#            return False
-
         for i in range(self.height-4,self.height): #negative diagonal
             for c in range(self.width-3):
                 if self.board[i][c] == self.board[i+1][c-1] == self.board[i+2][c-2] == self.board[i+3][c-3] != ' ':
                     return True
-
-#        else:
-#            return False
         return False
-                
     
     def is_full(self):
         count = 0
